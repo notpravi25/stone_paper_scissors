3f745e8981dd07bd41c9798d6d890af084a6a90b
notpravi25 <sunkaripravallika234@gmail.com>
2026-01-01 22:09:42 +0530

    Refactor app.py to include dynamic port configuration

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 import random
 app = Flask(__name__)
 
@@ -53,4 +54,5 @@ def stop():
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
