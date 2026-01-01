ea5e3b6c1ef60dcb26f9fee38db81e7a145706b4
notpravi25 <sunkaripravallika234@gmail.com>
2026-01-01 22:23:01 +0530

    Fix formatting issues in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import os
-from waitress import serve
 import random
+from waitress import serve
 app = Flask(__name__)
 
 @app.route("/")
@@ -57,4 +57,5 @@ def stop():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     serve(app, host="0.0.0.0", port=port)
+
 
