176277452bcd92ab11b9f075379cae2b42ec86ba
notpravi25 <sunkaripravallika234@gmail.com>
2026-01-01 22:16:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import os
+from waitress import serve
 import random
 app = Flask(__name__)
 
@@ -55,4 +56,5 @@ def stop():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    serve(app, host="0.0.0.0", port=port)
+
